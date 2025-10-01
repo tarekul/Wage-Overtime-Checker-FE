127739818324ed0b1f7d2f977e5629f148378c7e
tarekul <rahin6810@gmail.com>
2025-10-01 19:17:51 -0400

    remove console log

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ document.getElementById("payForm").addEventListener("submit", async (e) => {
     state: document.getElementById("state").value,
   };
 
-  console.log("test main branch");
-
   try {
     const res = await fetch(
       "https://wage-overtime-checker.onrender.com/check-pay",
